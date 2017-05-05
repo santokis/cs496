ae27c41ff667a94c2c9b9abea1bc7faf27aec46b
santokis <santokis@oregonstate.edu>
2017-05-04 22:37:48 -1000

    Updated

--- assignment4/demo.py
+++ assignment4/demo.py
@@ -1,39 +1,17 @@
-from __future__ import print_function
+from google.appengine.ext import ndb
+import logging
+import webapp2
+import json
 
-import sys
-
-from oauth2client import client
-from googleapiclient import sample_tools
-
-
-def main(argv):
-  # Authenticate and construct service.
-  service, flags = sample_tools.init(
-      argv, 'plus', 'v1', __doc__, __file__,
-      scope='https://www.googleapis.com/auth/plus.me')
-
-  try:
-    person = service.people().get(userId='me').execute()
-
-    print('Got your ID: %s' % person['displayName'])
-    print()
-    print('%-040s -> %s' % ('[Activitity ID]', '[Content]'))
-
-    # Don't execute the request until we reach the paging loop below.
-    request = service.activities().list(
-        userId=person['id'], collection='public')
-
-    # Loop over every activity and print the ID and a short snippet of content.
-    while request is not None:
-      activities_doc = request.execute()
-      for item in activities_doc.get('items', []):
-        print('%-040s -> %s' % (item['id'], item['object']['content'][:30]))
-
-      request = service.activities().list_next(request, activities_doc)
-
-  except client.AccessTokenRefreshError:
-    print ('The credentials have been revoked or expired, please re-run'
-      'the application to re-authorize.')
-
-if __name__ == '__main__':
-  main(sys.argv)
+class OauthHandler(webapp2.RequestHandler):
+	def get(self):
+		logging.debug('The contents of the GET request are:' + repr(self.request.GET))
+	
+class MainPage(webapp2.RequestHandler):
+	def get(self):
+		self.response.write("hello")
+		
+app = webapp2.WSGIApplication([
+	('/', MainPage),
+	('/oauth', OauthHandler)
+], debug=True)
---
+++ assignment4/main.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+</head>
+<body>
+	<div class="title">
+		<h1>CS 496 - Assignment 4</h1>
+		<h2>OAuth 2.0</h2>
+		<?php
+			echo 'Current date: '.date('Y-m-d-');
+		?>
+	</div>
+	<div>
+		<p>
+		Click on the link below.
+		</p>
+	</div>
+	<div>
+		<a href="https://cs496-gettingstarted-163606.appspot.com/oauth">Google Auth</a>
+	</div>
+</body>
+</html>
