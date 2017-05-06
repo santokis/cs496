e27127d800a52933cbd1275189d0140bc8fc2359
santokis <santokis@oregonstate.edu>
2017-05-06 09:45:33 -1000

    .

--- assignment4/demo.py
+++ assignment4/demo.py
@@ -5,13 +5,11 @@ import json
 
 class OauthHandler(webapp2.RequestHandler):
   def get(self):
-    get1 = repr(self.request.GET);
-    logging.debug('The contents of the GET request are:' + get1)
+    logging.debug('The contents of the GET request are:' + repr(self.request.GET));
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
 		self.response.write("hello");
-    	#self.response.write(get1)
 		
 app = webapp2.WSGIApplication([
 	('/', MainPage),
