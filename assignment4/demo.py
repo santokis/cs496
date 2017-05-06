358c392e46dcdd477b49fb7b06ac49039517b39d
santokis <santokis@oregonstate.edu>
2017-05-06 09:19:01 -1000

    .

---
+++ assignment4/demo.py
@@ -0,0 +1,20 @@
+from google.appengine.ext import ndb
+import logging
+import webapp2
+import json
+
+class OauthHandler(webapp2.RequestHandler):
+  def get(self):
+    get1 = repr(self.request.GET);
+    logging.debug('The contents of the GET request are:' + get1)
+    self.response.write(get1);
+
+class MainPage(webapp2.RequestHandler):
+	def get(self):
+		self.response.write("hello");
+    	#self.response.write(get1)
+		
+app = webapp2.WSGIApplication([
+	('/', MainPage),
+	('/oauth', OauthHandler)
+], debug=True)
